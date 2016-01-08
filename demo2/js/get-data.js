67c8ba03c808a715626e60bf14f68dfa48997618
Gaurav Agrawal <gauravagrawal13@users.noreply.github.com>
2016-01-08 11:09:33 +0530

    Create get-data.js

---
+++ demo2/js/get-data.js
@@ -0,0 +1,87 @@
+var app = angular.module("myApp", ['ngSanitize', 'ngCsv']);
+app.controller("GetUsers", function ($scope, $http) {
+    // this is where the JSON from api.php is consumed
+    $http.get('http://'+window.location.hostname+'/angular-demo/action/get-data.php').
+        success(function(data) {
+            // here the data from the get-data is assigned to a variable named users
+            $scope.users = data;
+        });
+        
+    //Post request to Insert record into the database.
+    $scope.insert = function () {
+ 
+            var request = $http({
+                method: "post",
+                url: 'http://'+window.location.hostname+'/angular-demo/action/insert.php',
+                data: {
+                    name: $scope.name,
+                    city: $scope.city
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+    $scope.responseMessage = "Inserted Successfuly !";
+    $scope.alertType = "success";
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    $scope.responseMessage = "Some Error Occured";
+    $scope.alertType = "danger";
+  });
+    }
+    
+            
+    //Post request to Delete record from database.    
+    $scope.remove = function ($id) {
+ 
+            var request = $http({
+                method: "post",
+                url: 'http://'+window.location.hostname+'/angular-demo/action/remove.php',
+                data: {
+                    id: $id
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+    
+    $scope.responseMessage = "Deleted Successfuly!";
+    $scope.alertType = "danger";
+    
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    $scope.responseMessage = "Some Error Occured";
+    $scope.alertType = "danger";
+  });
+    }    
+    
+    //Post request to Update record in database.
+     $scope.update = function ($id,$name,$city) {
+ 
+            var request = $http({
+                method: "post",
+                url: 'http://'+window.location.hostname+'/angular-demo/action/update.php',
+                data: {
+                    id: $id,
+                    name: $name,
+                    city: $city
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+    
+    $scope.responseMessage = "Updated Successfuly!";
+    $scope.alertType = "success";
+    
+  }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    $scope.responseMessage = "Some Error Occured";
+    $scope.alertType = "danger";
+  });
+    }    
+
+});
